6b5d9193fd87c43d30509b5609415564fd321895
mooveprince <mooveprince@gmail.com>
2017-03-02 06:29:47 -0500

    added Dear

--- src/com/simple/main/MainApplication.java
+++ src/com/simple/main/MainApplication.java
@@ -7,9 +7,10 @@ public class MainApplication {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Helper helper = new Helper ();
-		helper.greetMessage("Moove !!");
+		helper.greetMessage("Dear Moove !!");
 		helper.greetMessage("Thanks for using the service");
 
+	
 	}
 
 }
